929536ef651a6a537951babcb4e01b5989c0e655
Abhisheklellapalli <23P35A0409@ACET.AC.IN>
2025-07-12 12:00:06 +0530

    Create addition of binary numbers.c

---
+++ abhishek/addition of binary numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+ int a ,b;
+ int addition;
+ printf("Enter the first number:\n");
+ scanf("%b",&a);
+ printf("Enter second number:\n");
+ scanf("%b",&b);
+ addition = a + b;
+ printf("the answer is:%b",addition);
+    
+    return 0;
+}
