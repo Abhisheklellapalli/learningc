82308e0ec452863d6ce4fd9b5544cbb9abb0aac5
Abhisheklellapalli <23P35A0409@ACET.AC.IN>
2025-07-12 12:11:23 +0530

    Create subtraction of binary.c

---
+++ abhishek/subtraction of binary.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+ int a ,b;
+ int sub;
+ printf("Enter the first number:\n");
+ scanf("%b",&a);
+ printf("Enter second number:\n");
+ scanf("%b",&b);
+ sub = a - b;
+ printf("the answer is:%b",sub);
+    return 0;
+}
