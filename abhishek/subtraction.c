b2532af5143fda6bd68e49ed68f43d48e5b16e99
Abhisheklellapalli <23P35A0409@ACET.AC.IN>
2025-07-12 12:06:26 +0530

    Create subtraction.c

---
+++ abhishek/subtraction.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+ int a ,b;
+ int sub;
+ printf("Enter the first number:\n");
+ scanf("%d",&a);
+ printf("Enter second number:\n");
+ scanf("%d",&b);
+ sub = a - b;
+ printf("the answer is:%d",sub);
+    
+
+    return 0;
+}
