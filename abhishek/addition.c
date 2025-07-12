52f0ea3cf09b3c6e0e913ff58c3ab1785c3608e8
Abhisheklellapalli <23P35A0409@ACET.AC.IN>
2025-07-12 11:54:18 +0530

    Update and rename ok to addition.c

---
+++ abhishek/addition.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+ int a ,b;
+ int addition;
+ printf("Enter the first number:\n");
+ scanf("%d",&a);
+ printf("Enter second number:\n");
+ scanf("%d",&b);
+ addition = a + b;
+ printf("the answer is:%d",addition);
+    
+
+    return 0;
+}
