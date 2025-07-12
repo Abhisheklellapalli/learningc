340c6f374aa1e2e9f49a7e1934f2ba3b45c42cc2
Abhisheklellapalli <23P35A0409@ACET.AC.IN>
2025-07-12 12:18:24 +0530

    Create multiplication of binaries.c

---
+++ abhishek/multiplication of binaries.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+ int a ,b;
+ int mul;
+ printf("Enter the first number:\n");
+ scanf("%b",&a);
+ printf("Enter second number:\n");
+ scanf("%b",&b);
+ mul = a * b;
+ printf("the answer is:%b",mul);
+    return 0;
+}
