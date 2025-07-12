457e97f313dc71161af009a066cfee7543575ccc
Abhisheklellapalli <23P35A0409@ACET.AC.IN>
2025-07-12 12:14:33 +0530

    Create multiplication.c

---
+++ abhishek/multiplication.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+ int a ,b;
+ int mul;
+ printf("Enter the first number:\n");
+ scanf("%d",&a);
+ printf("Enter second number:\n");
+ scanf("%d",&b);
+ mul = a * b;
+ printf("the answer is:%d",mul);
+    return 0;
+}
